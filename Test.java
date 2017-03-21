2769939ea9e5511e8dfaac1c52e1d12b8ded798b
Wei Li <weili@Wei-2.fios-router.home>
2017-03-20 22:13:31 -0400

    add v1 branch sout

--- Test.java
+++ Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("this is testing");
         System.out.println("this is 2nd line");
+        System.out.println("this is v1 branch");
     }
 
 }
