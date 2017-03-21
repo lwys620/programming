a30155e013ed9da28cc20318531565144e3a3adb
Wei Li <weili@Wei-2.fios-router.home>
2017-03-20 22:09:07 -0400

    commit

--- Test.java
+++ Test.java
@@ -3,6 +3,7 @@ import java.io.*;
 public class Test {
     public static void main(String[] args) {
         System.out.println("this is testing");
+        System.out.println("this is 2nd line");
     }
 
 }
